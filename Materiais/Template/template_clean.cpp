e9b488bf63cb70d75f945f89c524612bbfd4029c
Nathália Pereira <np.nathaliapereira@gmail.com>
2022-09-02 05:27:08 -0300

    Adiciona otimização da decomposição de fatores primos e alguns outros algoritmos e corrige o nome do time

---
+++ Materiais/Template/template_clean.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -std=c++20 main.cpp
+
+// g++ -std=c++17 -Wshadow -Wall -Wextra -Wformat=2 -Wconversion -fsanitize=address,undefined -fno-sanitize-recover -Wfatal-errors
+
+// cout << fixed << setprecision(12) << value << endl;
+
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    
+ 
+    return 0;
+}
