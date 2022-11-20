cb48f251cf2af43f8efaf3ca75bed94249f3b500
Nathália Pereira <nathaliao@lacunasoftware.com>
2022-11-20 11:22:52 -0300

    Adiciona função de split e muda o nome de alguns arquivos

--- Materiais/Grafos/dijkstra.cpp
+++ Materiais/Grafos/dijkstra.cpp
@@ -38,3 +38,37 @@ vector<int> restore_path(int s, int t) {
     reverse(path.begin(), path.end());
     return path;
 }
+
+int adj[MAX][MAX];
+int dist[MAX];
+int minDistance(int dist[], bool sptSet[], int V) {
+    int min = INT_MAX, min_index;
+ 
+    for (int v = 0; v < V; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
+ 
+    return min_index;
+}
+
+void dijkstra(int src, int V) {
+ 
+    bool sptSet[V];
+    for (int i = 0; i < V; i++)
+        dist[i] = INT_MAX, sptSet[i] = false;
+ 
+    dist[src] = 0;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = minDistance(dist, sptSet, V);
+ 
+        sptSet[u] = true;
+ 
+        
+        for (int v = 0; v < V; v++)
+            if (!sptSet[v] && adj[u][v]
+                && dist[u] != INT_MAX
+                && dist[u] + adj[u][v] < dist[v])
+                dist[v] = dist[u] + adj[u][v];
+    }
+}
